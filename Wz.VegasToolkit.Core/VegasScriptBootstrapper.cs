059b690ca262ef8f34f0c3b27a20a4dc106ff5b0
吴从周 <wcz123694578@outlook.com>
2026-01-21 08:43:11 +0800

    依赖注入

---
+++ Wz.VegasToolkit.Core/VegasScriptBootstrapper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ScriptPortal.Vegas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wz.VegasToolkit.Core
+{
+    public abstract class VegasScriptBootstrapper
+    {
+        private readonly Vegas _vegas;
+
+        protected VegasScriptBootstrapper(Vegas vegas)
+        {
+            this._vegas = vegas;
+        }
+
+        protected IServiceProvider ServiceProvider { get; set; }
+
+        public void Run()
+        {
+            IServiceCollection service = new ServiceCollection();
+
+            ConfigureServices(service);
+
+            ServiceProvider = service.BuildServiceProvider();
+
+            OnStartup();
+        }
+
+        protected virtual void OnStartup()
+        {
+        }
+
+        protected virtual void ConfigureServices(IServiceCollection service)
+        {
+            service.AddSingleton(_vegas);
+        }
+    }
+}
